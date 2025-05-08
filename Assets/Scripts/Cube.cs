0b4492eaae5d9b4fcdfc457f8b302ea593da8def
JunDmitry <100911164+JunDmitry@users.noreply.github.com>
2025-05-08 20:08:07 +0300

    Remove Splitter refference and all click handle logic

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -4,29 +4,11 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     [SerializeField, Range(0f, 1f)] private float _splitChance;
-    [SerializeField] private Splitter _splitter;
 
     public float SplitChance => _splitChance;
 
-    public void Initialize(Splitter splitter, float splitChance)
+    public void Initialize(float splitChance)
     {
-        _splitter = splitter;
         _splitChance = splitChance;
-    }
-
-    private void OnMouseUpAsButton()
-    {
-        if (_splitter == null)
-            return;
-
-        if (IsSplit())
-            _splitter.Split(this);
-
-        Destroy(gameObject);
-    }
-
-    private bool IsSplit()
-    {
-        return _splitChance >= UnityEngine.Random.value;
     }
 }
