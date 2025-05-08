772ef00f673240db9c3ec8ded7c1d2201011df1b
JunDmitry <100911164+JunDmitry@users.noreply.github.com>
2025-05-08 20:05:45 +0300

    Remove Splitter refference and make Explode Public

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -2,23 +2,11 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    [SerializeField] private Splitter _splitter;
     [SerializeField] private float _force;
     [SerializeField] private float _radius;
 
-    private void OnEnable()
+    public void Explode(Rigidbody rigidbody)
     {
-        _splitter.CreatedChild += Explode;
-    }
-
-    private void OnDisable()
-    {
-        _splitter.CreatedChild -= Explode;
-    }
-
-    private void Explode(Cube cube, Vector3 parentPosition)
-    {
-        if (cube.gameObject.TryGetComponent(out Rigidbody rigidbody))
-            rigidbody.AddExplosionForce(_force, parentPosition, _radius);
+        rigidbody.AddExplosionForce(_force, rigidbody.transform.position, _radius);
     }
 }
