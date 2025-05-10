6edcdf8afd8d40b863f7c9029848a23a1e5ab872
JunDmitry <100911164+JunDmitry@users.noreply.github.com>
2025-05-10 03:43:50 +0300

    Add event Clicking, public method for interact after click on cube

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,10 +5,22 @@ public class Cube : MonoBehaviour
 {
     [SerializeField, Range(0f, 1f)] private float _splitChance;
 
+    public event Action<Cube> Clicking;
+
     public float SplitChance => _splitChance;
 
     public void Initialize(float splitChance)
     {
         _splitChance = splitChance;
+    }
+
+    public void InteractAfterClick()
+    {
+        Clicking?.Invoke(this);
+    }
+
+    private void OnDestroy()
+    {
+        Clicking = null;
     }
 }
